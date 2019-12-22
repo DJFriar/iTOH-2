d376c5e0ddec466516e76d0b86f27837f54d1bf7
DJFriar <me@tommyc.net>
2019-12-22 03:35:28 -0600

    Initial Commit

---
+++ Tour of Honor 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tour of Honor 2
+//
+//  Created by Tommy Craft on 12/22/19.
+//  Copyright © 2019 Tommy Craft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
