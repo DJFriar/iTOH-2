962c9cfaebf2df34d253ba9974c18b458a78cfd5
DJFriar <me@tommyc.net>
2020-03-16 00:35:10 -0500

    Updated icon set

---
+++ Tour of Honor 2/Onboarding/OnboardingView.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingView.swift
+//  Tour of Honor 2
+//
+//  Created by Tommy Craft on 3/16/20.
+//  Copyright © 2020 Tommy Craft. All rights reserved.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
